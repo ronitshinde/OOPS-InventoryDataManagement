97d1ef9e483d34dd5653013672c79b3e107857a2
ronitshinde <rony.shinde753@gmail.com>
2023-01-11 00:07:34 +0530

    Added Inventory Details

---
+++ FetchInventoryDetails.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace InventoryManagement
+{
+    public class FetchInventoryDetails
+    {
+        public Details Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path)) 
+            {
+                try
+                {
+                    String Json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Details>(Json);
+                }
+                catch(Exception e)
+                {
+                    return null;
+                }
+
+            }
+        }
+    }
+}
